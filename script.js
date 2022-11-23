8be879c02b2e4319e3d5ef9b4f186ef64eacdbed
ff <@ff>
2022-11-23 16:56:30 +0500

    hh

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var age = +prompt("Yoshingiz nechida")
 if (age > 59 && age <=150) {
     alert("Pensiyaga chiqqansiz, agar tirik bo`lsangiz")
-} else if (age > 50 && age <= 58) {
+} else if (age > 50 && age <= 59) {
     alert("Pensiya yoshiga oz qolibdi")
-} else if (age > 18 && age <= 49) {
+} else if (age > 18 && age <= 50) {
     alert("Hali yoshsiz , ishlashiz kerak")
-} else if (age > 0 && age <= 17) {
+} else if (age > 0 && age <= 18) {
     alert("Yoshingiz yetmaydi")
 } else {
     alert("Nimadir xato")
